d0070496a4ae009bbd1bae8028de20d93503ef3f
Tushar <tushar.dhake@yash.com>
2019-12-12 02:30:46 -0800

    Updates

---
+++ precache-manifest.744435ec857a3cb3dd4e3a6874d2651f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5305c4885c349d209796ef6c35898530",
+    "url": "/Monsters-Rolodex-ReactJS/index.html"
+  },
+  {
+    "revision": "5d64604a2f7ed4374ffe",
+    "url": "/Monsters-Rolodex-ReactJS/static/css/main.7aef24ec.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/Monsters-Rolodex-ReactJS/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex-ReactJS/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d64604a2f7ed4374ffe",
+    "url": "/Monsters-Rolodex-ReactJS/static/js/main.07cc65e8.chunk.js"
+  },
+  {
+    "revision": "66b62071a1c1dc43bcfc",
+    "url": "/Monsters-Rolodex-ReactJS/static/js/runtime-main.7d2cd356.js"
+  }
+]);
